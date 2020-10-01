ce4eb95a3e1568386f43d5b5866fdb4c58d24722
Nirankar1995 <58904103+Nirankar1995@users.noreply.github.com>
2020-10-02 01:40:19 +0530

    Update Capitalize.java
    Use the library function to convert the string in uppercase

--- src/jStrings/Capitalize.java
+++ src/jStrings/Capitalize.java
@@ -7,7 +7,7 @@ public class Capitalize {
 	public static void main(String[] args) {
 		Input.print("Cual es tu nombre? ");
 		String s = Input.get_string();
-	    if (s != null)
+	    /*if (s != null)
 	    {
 	        for (int i = 0, n = s.length(); i < n; i++)
 	        {
@@ -21,7 +21,12 @@ public class Capitalize {
 	            }
 	        }
 	        Input.print("\n");
-	    }
+	    }*/
+		
+		if(s != null) {
+			s = s.toUpperCase();
+			System.out.println(s);
+		}
 	}
 
 }
